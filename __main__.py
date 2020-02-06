6fca89c75c994c580abd930e1ba104c9fdb08610
patrickmorrell17 <patrick.morrell@wsu.edu>
2020-02-05 19:26:35 -0800

    Fixed the script- currently working

--- __main__.py
+++ __main__.py
@@ -4,11 +4,12 @@ import sys
 
 
 if len(sys.argv) == 3:
-    dir_path = pathlib.Path(sys.argv[1])
-    output_path_str = str(sys.argv[2]) + 'final.xlsx'
-    output_path = pathlib.Path(output_path_str)
-    final = Aggregator().read_dir(dir_path)
-    final.to_excel(output_path, index=False)
+    dir_path = pathlib.Path(sys.argv[1]) # Cast the first argument to a Path type
+    output_path_str = str(sys.argv[2]) + '\\final.csv' # Add in the name of the file to end of output path
+    output_path = pathlib.Path(output_path_str) # Cast ouput path string to Path type
+    final = Aggregator().read_dir(dir_path) # Aggregate
+    final.to_csv(output_path, index=False) # Output excel file to path specified
+
 else:
     print('Not enough arguments!')
 
@@ -18,5 +19,5 @@ else:
 
 
 
-#if __name__ == "__main__":
-#    main()
+# if __name__ == "__main__":
+#     main()
