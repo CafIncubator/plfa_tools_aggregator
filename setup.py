32c6fac80ba45a9216f7da84c44eff821152f35e
patrickmorrell17 <patrick.morrell@wsu.edu>
2020-03-02 12:47:56 -0800

    Updated setup.py file to be more accurate and included a long description. Also deleted setup.cfg

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
-from distutils.core import setup
+from setuptools import setup
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 setup(
   name = 'plfatools',         
   packages = ['plfatools'], 
@@ -11,14 +15,15 @@ setup(
   download_url = 'https://github.com/cafltar/plfa_tools_aggregator.git',   
   keywords = ['Aggregator', 'PLFA', 'CSV'],   
   install_requires=  ['pathlib', 'pandas', 'numpy'],
+  entry_points = {"console_scripts": ["plfatools=plfatools.__main__:main"]},
+  long_description = long_description,
+  long_description_content_type='text/markdown',
   classifiers=[
     'Development Status :: 4 - Beta',     
-    'Intended Audience :: PLFA Users',      
-    'Topic :: Data Aggregation :: Aggregator Tools',
-    'License :: OSI Approved :: CC0 License',  
-    'Programming Language :: Python :: 3',     
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
+		'Intended Audience :: Science/Research',      
+		'Topic :: Scientific/Engineering',
+		'Topic :: Scientific/Engineering :: Bio-Informatics',
+		'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',  
+		'Programming Language :: Python :: 3'
   ],
 )
